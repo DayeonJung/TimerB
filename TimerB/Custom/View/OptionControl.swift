5be1f4053ec2fe46b77520f5e2f92c015d83fc98
DayeonJung <ekdus0227@gmail.com>
2021-05-02 14:13:31 +0900

    feat Time, Player enum 내 변수 추가 및 클릭 시 text field 입력 가능하도록 구현

--- TimerB/Custom/View/OptionControl.swift
+++ TimerB/Custom/View/OptionControl.swift
@@ -10,6 +10,42 @@ import UIKit
 enum Option: String {
     case Time
     case Player
+    
+    var korean: String {
+        switch self {
+        case .Time:
+            return "시간"
+        case .Player:
+            return "인원 수"
+        }
+    }
+    
+    var maxNumber: Int {
+        switch self {
+        case .Time:
+            return 99
+        case .Player:
+            return 9
+        }
+    }
+    
+    var unit: Int {
+        switch self {
+        case .Time:
+            return 5
+        case .Player:
+            return 1
+        }
+    }
+    
+    var unitName: String {
+        switch self {
+        case .Time:
+            return "초"
+        case .Player:
+            return "명"
+        }
+    }
 }
 
 struct OptionModel {
@@ -47,6 +83,8 @@ class OptionControl: UIView {
     @IBOutlet weak var valueLabel: UILabel!
     @IBOutlet weak var minusButton: IconButton!
     @IBOutlet weak var plusButton: IconButton!
+    @IBOutlet weak var valueButton: UIButton!
+    
     
     override init(frame: CGRect) {
         super.init(frame: frame)
--- TimerB/Main/Cells/SelectOptionCell.swift
+++ TimerB/Main/Cells/SelectOptionCell.swift
@@ -43,6 +43,7 @@ class SelectOptionCell: UICollectionViewCell {
     }
     
     @IBOutlet weak var optionView: OptionControl!
+    var didClickValueButton: ((UIAlertController)->())?
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -52,25 +53,55 @@ class SelectOptionCell: UICollectionViewCell {
         self.optionView.plusButton.onClick = {
             
             let original = self.viewModel.option.currentValue()
-            let unit = self.viewModel.option.currentType() == .Player ? 1 : 5
-
-            let maxNum = self.viewModel.option.currentType() == .Player ? 9 : 99
+            let type = self.viewModel.option.currentType()
             
-            if original < maxNum {
-                self.viewModel.setNumber(with: min(original + unit, 99))
+            if original < type.maxNumber {
+                self.viewModel.setNumber(with: min(original + type.unit, type.maxNumber))
             }
         }
         
         self.optionView.minusButton.onClick = {
             
             let original = self.viewModel.option.currentValue()
-            let unit = self.viewModel.option.currentType() == .Player ? 1 : 5
+            let type = self.viewModel.option.currentType()
 
             if original >= 1 {
-                self.viewModel.setNumber(with: max(original - unit, 1))
+                self.viewModel.setNumber(with: max(original - type.unit, 1))
             }
         }
         
+        self.optionView.valueButton.addTarget(self, action: #selector(valueButtonClicked), for: .touchUpInside)
+        
+    }
+    
+    @objc private func valueButtonClicked() {
+        
+        let type = self.viewModel.option.currentType()
+        let title = type.korean + "을(를) 입력해주세요."
+        let placeHolderMessage = String(1) + "~" + String(type.maxNumber) + type.unitName + " 입력 가능"
+        let alert = UIAlertController(title: title,
+                                      message: nil,
+                                      preferredStyle: .alert)
+        alert.addTextField { (tf) in
+            tf.keyboardType = .numberPad
+            tf.placeholder = placeHolderMessage
+        }
+        
+        let ok = UIAlertAction(title: "확인", style: .default) { (ok) in
+            
+            let originalValue = self.viewModel.option.currentValue()
+            let input = alert.textFields?.first?.text ?? String(originalValue)
+            let number = min(Int(input) ?? originalValue, type.maxNumber)
+            self.viewModel.setNumber(with: number)
+        }
+        
+        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        
+        alert.addAction(cancel)
+        alert.addAction(ok)
+        
+        self.didClickValueButton?(alert)
+
     }
 
     func setViewModel(with vm: SelectOptionViewModel) {
